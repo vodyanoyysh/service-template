c693cd56619dc953151f7ffc2f65ae58da3d8574
d.a.kolesov <d.a.kolesov@s7.ru>
2024-08-13 09:10:21 +0700

    rename

--- template_service/api/router.py
+++ template_service/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from serivce.api.edpoints.system import system_router
+from template_service.api.edpoints.system import system_router
 
 router = APIRouter()
 router.include_router(system_router, tags=["system"])
